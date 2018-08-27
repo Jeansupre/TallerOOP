8c244b05248e17ef5e01c2a28028c79a624ca159
Jean Carlo <j.carlo0123@gmail.com>
2018-08-26 22:58:14 -0500

    Modificación Hola3

---
+++ git add Hola3.java
@@ -0,0 +1,30 @@
+[33mcommit f59bacb26542ae63c44cac159b1115809c6430a2[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m)[m
+Merge: b672193 d7e7e72
+Author: Jean Carlo <j.carlo0123@gmail.com>
+Date:   Sun Aug 26 22:50:09 2018 -0500
+
+    Merge branch 'master' of https://github.com/Jeansupre/TallerOOP
+
+[33mcommit d7e7e72a331cb191c95bc6493f6f5365968d62f9[m[33m ([m[1;31morigin/master[m[33m)[m
+Author: Daniel Castillo <danielalejandro1025@hotmail.com>
+Date:   Sun Aug 26 22:46:17 2018 -0500
+
+    Commit Hola3
+
+[33mcommit b6721931c3063d45dfb6158ae32dcaabe8353e1c[m
+Author: Jean Carlo <j.carlo0123@gmail.com>
+Date:   Sun Aug 26 22:45:32 2018 -0500
+
+    Commit Hola2
+
+[33mcommit 535b3c9dde57dc4c8d5b04808d95dcc127c65ad7[m
+Author: Jean Carlo <j.carlo0123@gmail.com>
+Date:   Sun Aug 26 22:38:19 2018 -0500
+
+    Commit primer archivo
+
+[33mcommit baab28382ed207d61e7810a4fc0f01b66fbcf995[m
+Author: Jeansupre <42504771+Jeansupre@users.noreply.github.com>
+Date:   Sun Aug 26 22:25:43 2018 -0500
+
+    Initial commit
